b85900a7b9f14a02db695234049542f05f5a7ff8
hoelog <dahoe0304@gmail.com>
2021-07-01 19:27:39 +0900

    test commit2

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,29 +1,31 @@
 import React, { useState } from 'react';
 
 function InputSample_2() {
-    const [inputs, setNumber] = useState(0);
+    const [num1, setNum1] = useState(0);
+    const [num2, setNum2] = useState(0);
 
-    const { num1, num2 } = inputs;
+    const onChangeNumber = (e) => {
+        const { num1, num2 } = e.target;
+    }
 
     const onPlus = () => {
-        console.log(setNumber(total => num1 + num2));
-
+        // alert(setInputs(num1 + num2));
+        alert(num1);
     };
 
     const onMinus = () => {
-        console.log(setNumber(num1 - num2));
+        // alert(setInputs(num1 - num2));
     }
 
     return (
-        <div>
-            <h1>계산기 연습문제 ➕ ➖</h1>
-            <input name="num1" type="number" placeholder="숫자1" value={num1} />
-            <input name="num2" type="number" placeholder="숫자2" value={num2} />
-            <button onClick={onPlus}>+</button>
-            <button onClick={onMinus}>-</button>
-            <div>
-            </div>
-        </div>
+        // <div>
+        //     <h1>계산기 연습문제 ➕ ➖</h1>
+        //     <input type="number" placeholder="숫자1" value={num1} onChange={onChangeNumber} />
+        //     <input type="number" placeholder="숫자2" value={num2} onChange={onChangeNumber} />
+        //     <button onClick={onPlus}>+</button>
+        //     <button onClick={onMinus}>-</button>
+        // </div>
+        <div>test</div>
     );
 }
 

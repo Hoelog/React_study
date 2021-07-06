6b90491e70064b3cf09e236dd0bb779534a13ee4
hoelog <dahoe0304@gmail.com>
2021-07-07 01:24:08 +0900

    router homework

---
+++ src/component/About.js
@@ -0,0 +1,22 @@
+// 잘콩님이랑 한 소스
+// export default function About() {
+//     return (
+//         <div>
+//             <h1>소개합니다</h1>
+//             <p>라우터 실습중입니다 뚠뚠</p>
+//         </div>
+//     );
+// }
+
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>소개</h1>
+            <p>여기는 소개 페이지 입니다. 제일 좋아하는 회는 참치회 소곱창 먹는다 흑흑,,,</p>
+        </div>
+    );
+};
+
+export default About;
